739c0ed005afa9030d22b324c9c8fdd8fd5a1047
hmeyd <yyahmedsalm@gmail.com>
2025-01-16 11:31:46 +0100

    ajouter une ligne pour afficher erreur

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,5 +1,9 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 import sys
-
-for i, arg in enumerate(sys.argv):
-    print(f"Argument {i}: {arg}")
+# Vérifie si des arguments ont été passés
+if len(sys.argv) > 1:
+    # Parcourt les arguments en ignorant le nom du script
+    for i in range(1, len(sys.argv)):
+        print(sys.argv[i])
+else:
+    print("Aucun argument n'a été fourni.")
