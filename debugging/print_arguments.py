c67ec7e6fdcd9e3dfd063c15369c89a9f9f78891
hmeyd <yyahmedsalm@gmail.com>
2025-01-16 11:24:29 +0100

    print_arguments

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-def print_arguments(arguments):
-    """Affiche les arguments passés au script."""
-    for index, arg in enumerate(arguments):
-        print(f"Argument {index}: {arg}")
-
-if __name__ == "__main__":
-    print_arguments(sys.argv)
+for i, arg in enumerate(sys.argv):
+    print(f"Argument {i}: {arg}")
